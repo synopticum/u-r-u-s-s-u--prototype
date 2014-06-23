36d3cd65a6b581aa4697efe5ac4fb4e54139e5c3
Synopticum <synopticum@gmail.com>
2014-06-23 18:34:54 +0400

    create / save / destroy / update model

--- build/js/init.js
+++ build/js/init.js
@@ -8,7 +8,10 @@ var LeafIcon = L.Icon.extend({
 
 // model constructor
 var Dots = function (data) {
-    this.records = data;
+    this.records = {};
+    for (var item in data) {
+        this.records[item] = new Dot(data[item]);
+    }
 };
 
 // dot constructor
@@ -17,17 +20,31 @@ Dot = function (item) {
 };
 
 Dot.prototype = {
+    newRecord : true,
+    create: function () {
+        this.newRecord = false;
+        dots.records[this.id] = this;
+    },
+    update: function () {
+        dots.records[this.id] = this;
+    },
+    save: function () {
+        this.newRecord ? this.create() : this.update();
+    },
+    destroy: function () {
+        delete dots.records[this.id];
+    },
     imageDefault: 'data:image/gif;base64,R0lGODlhUABQALMAAAAAAGtra/T09JycnKqqqtHR0SYmJunp6bi4uI2NjX19fcXFxUJCQlhYWN3d3f///yH5BAAAAAAALAAAAABQAFAAAATG8MlJq7046827/2AojmRpnmiqrmzrvnAsz3Rt33iu73zv/8CgcEgsGo/IpHLJbDqf0KhQgBgEAgrCQVpJAL7g70DAlTDC6ET5QQgsyIJFA1wokysCg3h9GXwDfBYEf4EVAV8EhRMFYFuKAnMAaooPCl8Gd4VeX3WKC2AIlA9nAAOiCF8MmYWWpaIPhwCdlLGzilYBDq+7vBUHBKGiB3qulIOXoscABqKQX8GiBY691NXW19jZ2tvc3d7f4OHi4+Tl5ufo6UARADs=',
     icons: {
-        red  : new LeafIcon({iconUrl: 'js/leaflet/images/markers/marker-icon-pink.png'}),
-        blue : new LeafIcon({iconUrl: 'js/leaflet/images/markers/marker-icon-blue.png'}),
+        red: new LeafIcon({iconUrl: 'js/leaflet/images/markers/marker-icon-pink.png'}),
+        blue: new LeafIcon({iconUrl: 'js/leaflet/images/markers/marker-icon-blue.png'}),
         green: new LeafIcon({iconUrl: 'js/leaflet/images/markers/marker-icon-green.png'})
     },
     popupTemplate: function (context) {
         var dot = context;
 
         return '<div class="dot-view">'
-            + '<a href="gallery/' + dot.id + '/1.jpg" class="fancybox" data-fancybox-group="gallery' + dot.id + '"><img src="' + dot.image + '" class="dot-image"></a>'
+            + '<a href="gallery/' + dot.id + '/1.jpg" class="fancybox" data-fancybox-group="gallery' + dot.id + '"><img src="' + dot.getImage() + '" class="dot-image"></a>'
             + '<a href="gallery/98/2.jpg" class="fancybox" data-fancybox-group="gallery98" style="display:none;"></a>'
             + '<a href="feeds/' + dot.id + '" class="dot-chat"></a>'
             + '</div>'
@@ -65,9 +82,19 @@ Dot.prototype = {
     }
 };
 
-/* map initialize */
-var dots = {};
-var dotsReady = [];
+// id generator
+Math.guid = function () {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g,
+
+    function (c) {
+        var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
+    }).toUpperCase()
+};
+
+/* map initialization */
+var dots = {};      // model
+var dotsReady = []; // leaflet.js markers array
 
 $.ajax({
     type: "GET",
@@ -77,14 +104,8 @@ $.ajax({
         dots = new Dots(JSON.parse(data));
 
         for (var item in dots.records) {
-            var dot = new Dot(dots.records[item]);
-
-            dot.id = Number(dot.id);
-            dot.byUser = Boolean(dot.byUser);
-            dot.icon = dot.getIcon();
-            dot.image = dot.getImage();
-
-            var marker = L.marker(dot.position, { icon: dot.icon }).bindPopup(dot.popupTemplate(dot));
+            var dot = dots.records[item];
+            var marker = L.marker(dot.position, { icon: dot.getIcon() }).bindPopup(dot.popupTemplate(dot));
             dotsReady.push(marker);
         }
 
@@ -106,7 +127,10 @@ function initialize() {
     }).setView([70, 10], 5);
 
     // map draggable area
-    map.setMaxBounds([[5, -180], [122, 100]]);
+    map.setMaxBounds([
+        [5, -180],
+        [122, 100]
+    ]);
 
     // map size
     var mapBounds = new L.LatLngBounds(
@@ -128,47 +152,3 @@ function initialize() {
         $('.input-position', that).val([e.latlng.lat, e.latlng.lng]).attr('disabled', 'disabled');
     });
 }
-
-// add dot
-
-/* ready */
-$(document).ready(function () {
-    $('#clouds').fadeOut(2000);
-    $('.fancybox').fancybox();
-    $(".selectbox").selectbox();
-
-    $('#placeDot form').on('submit', function () {
-        return false;
-    });
-
-    $('#placeDotNow').on('click', function () {
-        that = $('#placeDot');
-
-        that.position    = $(".input-position", that).val().split(',');
-        that.title       = $(".input-title", that).val();
-        that.shortText   = $(".input-short-text", that).val();
-        that.image       = $(".input-image", that).val();
-        that.icon        = $(".input-icon", that).val();
-        that.address     = $(".input-address", that).val();
-        that.homePhone   = $(".input-home-phone", that).val();
-        that.mobilePhone = $(".input-mobile-phone", that).val();
-
-        var dot = new Dot({
-            id          : null,
-            template    : null,
-            byUser      : null,
-            position    : that.position,
-            title       : that.title,
-            shortText   : that.shortText,
-            image       : that.image,
-            icon        : that.icon,
-            address     : that.address,
-            homePhone   : that.homePhone,
-            mobilePhone : that.mobilePhone
-        });
-
-        L.marker(dot.position, { icon: dot.getIcon() }).bindPopup(dot.popupTemplate(dot)).addTo(map);
-
-        $.fancybox.close(that);
-    });
-});
